a9f5e8accc39a7180b42f16170f93a5f31e73f0d
Annie <annie.gallagher7@gmail.com>
2023-01-23 11:52:42 -0700

    added routes for tasks

--- app.rb
+++ app.rb
@@ -11,3 +11,13 @@ end
 get '/' do
     erb :tasks, layout: :index
 end
+
+patch '/tasks/:id' do
+    @tasks.all[params[:id].to_i].mark_as_done!
+    erb :tasks
+end
+
+delete '/tasks/:id' do
+    @tasks.all.delete_at(params[:id].to_i)
+    erb :tasks
+end
